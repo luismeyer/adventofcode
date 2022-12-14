8efdca9706f817beb7081672c167c1cee3892abe
Luis Meyer <luis.meyer@accenture.com>
2022-12-14 22:46:38 +0100

    10.2

--- 10.ts
+++ 10.ts
@@ -18,170 +18,17 @@ const prepare = (lines: string[]): CleanCmd[] => {
   }, []);
 };
 
-const Part1Cycles = new Set([20, 60, 100, 140, 180, 220]);
+const MeasureCycles = new Set([20, 60, 100, 140, 180, 220]);
 
-const test = `
-addx 15
-addx -11
-addx 6
-addx -3
-addx 5
-addx -1
-addx -8
-addx 13
-addx 4
-noop
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx -35
-addx 1
-addx 24
-addx -19
-addx 1
-addx 16
-addx -11
-noop
-noop
-addx 21
-addx -15
-noop
-noop
-addx -3
-addx 9
-addx 1
-addx -3
-addx 8
-addx 1
-addx 5
-noop
-noop
-noop
-noop
-noop
-addx -36
-noop
-addx 1
-addx 7
-noop
-noop
-noop
-addx 2
-addx 6
-noop
-noop
-noop
-noop
-noop
-addx 1
-noop
-noop
-addx 7
-addx 1
-noop
-addx -13
-addx 13
-addx 7
-noop
-addx 1
-addx -33
-noop
-noop
-noop
-addx 2
-noop
-noop
-noop
-addx 8
-noop
-addx -1
-addx 2
-addx 1
-noop
-addx 17
-addx -9
-addx 1
-addx 1
-addx -3
-addx 11
-noop
-noop
-addx 1
-noop
-addx 1
-noop
-noop
-addx -13
-addx -19
-addx 1
-addx 3
-addx 26
-addx -30
-addx 12
-addx -1
-addx 3
-addx 1
-noop
-noop
-noop
-addx -9
-addx 18
-addx 1
-addx 2
-noop
-noop
-addx 9
-noop
-noop
-noop
-addx -1
-addx 2
-addx -37
-addx 1
-addx 3
-noop
-addx 15
-addx -21
-addx 22
-addx -6
-addx 1
-noop
-addx 2
-addx 1
-noop
-addx -10
-noop
-noop
-addx 20
-addx 1
-addx 2
-addx 2
-addx -6
-addx -11
-noop
-noop
-noop
-`;
-
-challenge(10, (rawInput) => {
-  const lines = rawInput.split("\n").filter(Boolean);
-
+const simpleCPU = (cmds: CleanCmd[]) => {
   let x = 1;
-  let part1 = 0;
-
-  const cmds = prepare(lines);
+  let strength = 0;
 
   cmds.forEach((value, index) => {
     const clock = index + 1;
 
-    if (Part1Cycles.has(clock)) {
-      part1 = part1 + x * clock;
+    if (MeasureCycles.has(clock)) {
+      strength = strength + x * clock;
     }
 
     if (typeof value === "number") {
@@ -189,5 +36,44 @@ challenge(10, (rawInput) => {
     }
   });
 
+  return strength;
+};
+
+const draw = (cmds: CleanCmd[], lineLength: number) => {
+  let line = "";
+
+  let x = 1;
+
+  cmds.forEach((cmd) => {
+    const lineIndex = line.length;
+
+    const drawSprite =
+      lineIndex === x || lineIndex - 1 === x || lineIndex + 1 === x;
+
+    const char = drawSprite ? "X" : ".";
+
+    line = line + char;
+
+    if (line.length === lineLength) {
+      console.log(line);
+      line = "";
+    }
+
+    if (typeof cmd === "number") {
+      x = x + cmd;
+    }
+  });
+};
+
+challenge(10, (rawInput) => {
+  const lines = rawInput.split("\n").filter(Boolean);
+  const cmds = prepare(lines);
+
+  const part1 = simpleCPU(cmds);
+
   console.log("Part1", part1);
+
+  const lineLength = 40;
+
+  draw(cmds, lineLength);
 });
